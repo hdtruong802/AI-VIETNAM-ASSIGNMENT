9456cc673acfcca83ad54347942304ff8ea12848
hdtruong802 <ductruong802@gmail.com>
2025-03-17 20:25:10 +0700

    update day7

--- test.py
+++ test.py
@@ -1,20 +1,2 @@
-def sum_perfect_numbers(N):
-    if N >= 2:
-        perfect_numbers = []
-        for num in range(2, N):
-            divisors = []
-            for i in range(1, num):
-                if num % i == 0:
-                    divisors.append(i)
-            if sum(divisors) == num:
-                perfect_numbers.append(num)
-        
-        return sum(perfect_numbers)
-    else:
-        return 0
+numbers = [10, 20, 30, 40, 50]
 
-# Nhập giá trị N từ người dùng
-# N = int(input("Nhập N: "))
-N = 30
-result = sum_perfect_numbers(N)
-print(f"Tổng các số hoàn hảo nhỏ hơn {N}: {result}")
